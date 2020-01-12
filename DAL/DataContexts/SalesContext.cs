5d6dffa930ee8be008325d526d83e6909b2d9848
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-01-12 17:27:24 +0300

    dal: delete project DAL

---
+++ DAL/DataContexts/SalesContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace DAL.DataContexts
+{
+    public class SalesContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<FileName> Files { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<TmpSale> TmpSales { get; set; }
+
+        public SalesContext()
+            : base("name=SalesContext")
+        {
+        }
+    }
+}
