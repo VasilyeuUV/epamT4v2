e6705a6630f89ec1798253534cc7648d8983656f
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-01-12 15:50:41 +0300

    db: context

--- EFCF/DataContexts/SalesContext.cs
+++ EFCF/DataContexts/SalesContext.cs
@@ -1,13 +1,20 @@
 ﻿namespace EFCF.DataContexts
 {
-    using System;
+    using EFCF.DataModels;
     using System.Data.Entity;
-    using System.Linq;
 
     public class SalesContext : DbContext
     {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<FileName> Files { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<TmpSale> TmpSales { get; set; }
+
         public SalesContext()
             : base("name=SalesContext")
         {
         }
+    }
 }
