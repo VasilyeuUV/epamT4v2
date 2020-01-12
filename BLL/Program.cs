3a659e2365da28fc3536a15f02c3fa324706ac37
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-01-12 11:38:05 +0300

    bll: delete from main

--- BLL/Program.cs
+++ BLL/Program.cs
@@ -9,40 +9,9 @@ namespace BLL
 {
     class Program
     {
-        private static readonly object locker = new object();
-
-        static int Summ(object obj)
-        {
-            int n = (int)obj;
-            //lock (locker) { 
-                ++n;
-                Console.WriteLine($"TaskId: {Task.CurrentId}  write {n}");
-            //}
-            return n;
-        }
-
-
+        
         static void Main(string[] args)
-        {
-            List<Thread> tasks = new List<Thread>();
-            for (int i = 0; i < 100; i+=10)
-            {
-                //lock (locker)
-                //{
-                    Thread thread = new Thread(new ThreadStart(Summ));
-                    tasks.Add(thread);
-                //}
-            }
-
-            foreach (var task in tasks)
-            {
-                task.Start();
-                Console.WriteLine($"TaskId: {Task.CurrentId}  write {task.Result}");
-            }
-
-            Console.ReadKey();
+        {          
         }
-
-
     }
 }
