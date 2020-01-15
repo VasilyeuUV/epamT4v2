51c357b5aa24418c4de017cdce4dc5a0f8e994d6
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-01-15 08:18:54 +0300

    pl: create asp.net mvc project

---
+++ plAspNetWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(plAspNetWebApp.Startup))]
+namespace plAspNetWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
